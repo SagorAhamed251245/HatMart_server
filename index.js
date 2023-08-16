f395d396dd12ab07d4445781a422551586a0e77a
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-08-17 00:42:18 +0600

    product route done

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-const  express = require('express'); 
+const productRoutes = require("./routes/productRoutes");
+const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const cors = require('cors');
-require('dotenv').config()
+const cors = require("cors");
+require("dotenv").config();
 
-mongoose.connect(process.env.URL).then(()=> console.log('database connected..')).catch(err => console.log(err))
+mongoose
+  .connect(process.env.URL)
+  .then(() => console.log("database connected.."))
+  .catch((err) => console.log(err));
 
-// server medelwere 
-app.use(cors())
-app.use(express.json())
+// middleware
+app.use(cors());
+app.use(express.json());
 
-// route 
-app.get("/",()=> {
-    resizeBy.send('hatmart project')
+// route
+app.get("/", (req, res) => {
+  res.send("hatmart project");
 });
 
+app.use("/api/v1/auth", productRoutes);
 
-// server listen 
-const PORT = process.env.PORT;
-app.listen(PORT, ()=>{
-    console.log("server listening... " + PORT)
-})
+// server listen
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server listening...  ${PORT}`);
+});
