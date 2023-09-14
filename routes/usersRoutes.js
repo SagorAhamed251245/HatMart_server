3c94bec8fa1fa78efcf6784124930eb1a140e562
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-09-14 22:40:42 +0600

    added change user role

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -4,6 +4,7 @@ const {
   getAllUser,
   getSingleUser,
   editUser,
+  changeUserRole,
 } = require("../controller/userModelController");
 const verifyAdmin = require("../middleware/verifyAdmin");
 const router = express.Router();
@@ -12,6 +13,7 @@ router.post("/users", createUser);
 router.get("/users", getAllUser);
 router.get("/user/:email", getSingleUser);
 router.patch("/updateUser/:id", editUser);
+router.patch("/changeUserRole/:id", changeUserRole);
 router.get("/userId/:id", editUser);
 
 module.exports = router;
